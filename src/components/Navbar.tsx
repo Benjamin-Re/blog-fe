2b84c363266dbed4ff6ece35310fa518f72dcbf3
brenker <brenker@theworkshop.com>
2026-06-04 18:05:30 +0200

    add logout button to navbar and update readme

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,20 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import styles from "./Navbar.module.css"
 
+
 export function Navbar() {
+    const navigate = useNavigate()
+    async function handleLogout() {
+        await fetch('http://localhost:3000/users/logout', {method: 'GET', credentials: 'include'})
+        navigate('/')
+    }
+
     return (
         <div className={styles.navContainer}>
             <Link to="/">Home</Link>
             <Link to="/users/login">Login</Link>
             <Link to="/users/signup">Signup</Link>
+            <button onClick={handleLogout}>Logout</button>
         </div>
     )
 }
