d05ec89f43ac979f0c7ad7c59ecea15e1161d585
brenker <brenker@theworkshop.com>
2026-06-10 17:18:17 +0200

    display messages on homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,24 @@
+import { useEffect, useState } from 'react'
 import { Greeting } from '../components/Greeting'
+
+
 export function HomePage () {
+    const [messages, setMessages] = useState<{ id: number; title: string; content: string }[]>([])
+
+    useEffect(()=>{
+        async function getMessages() {
+            const raw = await fetch('http://localhost:3000/messages')
+            const data = await raw.json()
+            return data
+        }
+        getMessages().then(setMessages)
+    }, [])
     return (
         <>
             <Greeting></Greeting>
+            <div>
+                { messages.map(m => <div key={m.id}><h3>{m.title}</h3><p>{m.content}</p></div>) }
+            </div>
         </>
     )
 }
