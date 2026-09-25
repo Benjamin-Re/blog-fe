55997ff3ca06159272e0177ce3d03bf52d1e7f66
brenker <brenker@theworkshop.com>
2026-09-25 10:33:28 +0200

    Add second Pact

--- src/api/posts.pact.spec.ts
+++ src/api/posts.pact.spec.ts
@@ -1,5 +1,5 @@
 import { PactV3, MatchersV3 } from "@pact-foundation/pact";
-import { listPosts } from "./posts";
+import { listPosts, createPost } from "./posts";
 
 const { eachLike, like } = MatchersV3;
 
@@ -22,13 +22,42 @@ describe("GET /posts", () => {
           timestamp: like("2026-01-01T00:00:00Z"),
         }),
       });
-      return pact.executeTest(async (mockServer) => {
-        vi.stubEnv('VITE_API_BASE_URL',mockServer.url)
-        const posts = await listPosts();
-        expect(posts).toHaveLength(1)
-        expect(posts[0]).toMatchObject({ id: 1, title: "My Post", content: "My Content", timestamp: "2026-01-01T00:00:00Z" })
+    return pact.executeTest(async (mockServer) => {
+      vi.stubEnv("VITE_API_BASE_URL", mockServer.url);
+      const posts = await listPosts();
+      expect(posts).toHaveLength(1);
+      expect(posts[0]).toMatchObject({
+        id: 1,
+        title: "My Post",
+        content: "My Content",
+        timestamp: "2026-01-01T00:00:00Z",
+      });
+    });
+  });
+});
 
+describe("POST /posts/create", () => {
+  it("returns ok on create post", () => {
+    pact
+      .uponReceiving("a post request to create a new post")
+      .withRequest({
+        method: "POST",
+        path: "/posts/create",
+        body: { title: "My Title", content: "My Content" },
       })
+      .willRespondWith({
+        status: 200,
+        body: like({ title: "My Title", content: "My Content"})
+      });
+    const token = "test-token"
+    return pact.executeTest(async (mockServer) => {
+      vi.stubEnv("VITE_API_BASE_URL", mockServer.url);
+      const res = await createPost(
+        { title: "My Title", content: "My Content" },
+        token,
+      );
+      expect(res).toBeTruthy()
+    });
   });
 });
 
